4c5fefadad3cbedf0657851fca9409832999fa90
orlinromy <orlinromy@gmail.com>
2023-05-14 20:44:43 +0800

    fix: db connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,11 +3,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: process.env.POSTGRES_DB_NAME,
+  connectionString: process.env.POSTGRES_CONNECTION_LINK,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
